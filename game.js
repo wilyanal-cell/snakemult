b2be87c90cb2a263179f77c182988b8eaf14c43d
wilyanal-cell <wilyan.al@hotmail.com>
2026-09-11 16:38:57 -0300

    Update game.js

--- game.js
+++ game.js
@@ -24,7 +24,7 @@ document.getElementById('againButton').onclick=start;
 
 function connect(name){
   // Configure your deployed WebSocket URL here:
-  const WS_URL=window.snakemult.onrender.com||'';
+  const WS_URL = window.SNAKE_WS_URL || 'wss://snakemult.onrender.com';
   if(WS_URL){
     try{
       ws=new WebSocket(WS_URL);
